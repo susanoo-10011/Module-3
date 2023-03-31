8db25b8f64b145e516f572010cd42ddd5dcf6141
susanoo-10011 <cycahosir@gmail.com>
2023-03-31 22:13:11 +0700

    Поменял Program.

---
+++ HomeWork 3.3.2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork._3._3._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName = "Egor";
+            byte MyAge = 19;
+            bool HaveIApet = false;
+            double MyShoeSize = 41.5;
+
+            Console.WriteLine($"My name is {MyName}");
+            Console.WriteLine($"MyAge {MyAge}");
+            Console.WriteLine($"Do I have a pet? {HaveIApet}");
+            Console.WriteLine($"My shoe size is {MyShoeSize}");
+
+            Console.ReadKey();
+        }
+    }
+}
